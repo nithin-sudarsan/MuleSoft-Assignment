f5e8034ec9d27d7033822ddfee4d98507726dca2
nithin-sudarsan <84195790+nithin-sudarsan@users.noreply.github.com>
2022-04-14 08:48:23 +0530

    commit_2

--- movies_sqlite3.py
+++ movies_sqlite3.py
@@ -4,39 +4,40 @@ with sqlite3.connect("movies_database.db") as db:
     cursor=db.cursor()
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS movies(
+CREATE TABLE IF NOT EXISTS Movies(
 Movie_name TEXT NOT NULL,
 Lead_actor TEXT NOT NULL,
 Lead_actress TEXT NOT NULL,
 Director_name TEXT NOT NULL,
-Year_of_release INTEGER NOT NULL,
-);
+Year_of_release INTEGER NOT NULL);
 ''')
 
-insert_movie_1='''INSERT INTO movies(question) VALUES(?)'''
-cursor.execute(insert_question, [(quest)])
-db.commit()
-insert_movie_2='''INSERT INTO movies(question) VALUES(?)'''
-cursor.execute(insert_question, [(quest)])
-db.commit()
-insert_movie_3='''INSERT INTO movies(question) VALUES(?)'''
-cursor.execute(insert_question, [(quest)])
-db.commit()
-insert_movie_4='''INSERT INTO movies(question) VALUES(?)'''
-cursor.execute(insert_question, [(quest)])
-db.commit()
-insert_movie_5='''INSERT INTO movies(question) VALUES(?)'''
-cursor.execute(insert_question, [(quest)])
-db.commit()
-insert_movie_6='''INSERT INTO movies(question) VALUES(?)'''
-cursor.execute(insert_question, [(quest)])
+insert_movies='''INSERT INTO Movies (Movie_name, Lead_actor, Lead_actress, Director_name, Year_of_release) VALUES 
+("Inception", "Leonardo DiCaprio", "Elliot Page", "Christopher Nolan", 2010),
+("Jurrasic World", "Chris Pratt", "Bryce Dallas", "J.A Bayona", 2018),
+("The Nice Guys", "Ryan Gosling", "Margaret Qualley", "Shane Black", 2016),
+("Aladdin", "Will Smith", "Naomi Scott", "Guy Ritchie", 2019),
+("Spiderman - No way Home", "Tom Holland", "Zendaya", "Jon Watts", 2021),
+("The Mask", "Jim Carrey", "Amy Yasbeck", "Chuck Russell", 1994)'''
+cursor.execute(insert_movies)
 db.commit()
 
-#or use single insert statement for everything
+def get_movies():
+    query_1='''SELECT * FROM Movies'''
+    cursor.execute(query_1)
+    rows=cursor.fetchall()
+    for row in rows:
+        print(row)
 
-query='''SELECT Movie_name FROM movies WHERE Lead_actor='actor name' '''
-cursor.execute(query) 
-quest=cursor.fetchall()
-db.commit()
 
-cursor.execute("SELECT * FROM movies")
+get_movies()
+
+
+def get_movie(actor_name):
+    query_2='''SELECT Movie_name FROM Movies WHERE Lead_actor=(?) '''
+    cursor.execute(query_2, [(actor_name)]) 
+    movie_name=cursor.fetchall()
+    for  name in movie_name:
+        print("The lead actor of ",name[0], "is", actor_name)
+
+get_movie('Will Smith')
